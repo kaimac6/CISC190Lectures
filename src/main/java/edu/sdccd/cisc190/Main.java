dcd85a79391565bb01d6ef0d1b2e9cd4dddf443e
Kai Mac <kaiatwork6@gmail.com>
2024-02-15 01:44:35 -0800

    initial commit

---
+++ src/main/java/edu/sdccd/cisc190/Main.java
@@ -0,0 +1,24 @@
+package edu.sdccd.cisc190;
+
+public class Main {
+    public static void main(String[] args) {
+        int integerDemo = 1;
+        System.out.println(integerDemo);
+
+        long longDemo = 9999999999L;
+
+        System.out.println(longDemo);
+        System.out.println(longDemo + 200);
+        System.out.println(longDemo - 200);
+        System.out.println(longDemo / 5);
+        System.out.println(longDemo * 25);
+
+//        byte
+//        long
+//        float
+//        double
+
+        System.out.println("hello world!");
+
+    }
+}
